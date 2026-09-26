ed60a2db61647b9008b48f54578b4304267a6d34
@karentat09 <ktmartinez@unimayor.edu.co>
2026-09-25 21:55:27 -0500

    SCRUM 35

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
+const registerRoutes = require('./routes/register.routes');
 const walletRoutes = require('./routes/wallet.routes'); // nuevo
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api', authRoutes);
 app.use('/api', walletRoutes); // nuevo
+app.use('/api/auth', registerRoutes);
 
 app.get('/', (req, res) => {
   res.json({ mensaje: 'API funcionando' });
